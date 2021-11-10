929835b69534cb0baf0790cbd4b090f04862bda1
Arceusir <aaronchristopher.dalmacio@gmail.com>
2021-11-10 01:42:49 +0000

    Implementation of Docker

--- app.sh
+++ app.sh
@@ -9,14 +9,16 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5000" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "RUN python3 -m pip install flask" >> tempdir/Dockerfile
+echo "RUN python3 -m pip install urllib3" >> tempdir/Dockerfile
+echo "RUN python3 -m pip install requests" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t app .
-docker run -t -d -p 5000:5000 --name running app
+docker run -t -d -p 5050:5050 --name apprunning app
 docker ps -a
